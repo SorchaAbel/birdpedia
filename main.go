6659ab2282ca5906234e3ed495469949ca7b78a3
Sorcha Abel <sorchajkeane@gmail.com>
2018-11-12 21:11:28 +1000

    Golang using mux

---
+++ main.go
@@ -0,0 +1,27 @@
+// main.go project main.go
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	r := newRouter()
+	http.ListenAndServe(":8080", r)
+
+}
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World!")
+}
+
+func newRouter() *mux.Router {
+	r := mux.NewRouter()
+	r.HandleFunc("/hello", handler).Methods("GET")
+	staticFileDirectory := http.Dir("./assets/")
+	staticFileHandler := http.StripPrefix("/assets/", http.FileServer(staticFileDirectory))
+	r.PathPrefix("/assets/").Handler(staticFileHandler).Methods("GET")
+	return r
+}
